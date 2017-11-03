66e1824928ead5411693bfc12cf3d64bdac0b566
Michal <238181@student.pwr.edu.pl>
2017-11-03 04:57:21 +0100

    Refactor

--- src/main/java/me/mjaroszewicz/Application.java
+++ src/main/java/me/mjaroszewicz/Application.java
@@ -7,9 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
-    //TODO - split js
     //TODO - add aesthetic 'send' functionality
-    //TODO - css
 
     public static void main(String[] args){
         SpringApplication.run(Application.class);
